8373aa95dc5e8e3d946daeeefb26b90a59ca1662
현호 <bflhh1@gmail.com>
2021-11-08 18:38:20 +0900

    index페이지 아이디 비밀번호 input창 클릭시 이벤트 추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,14 @@
-document.getElementsByClassName("input-text").
+//아이디 포커스 이벤트
+document.getElementById("id_text").onfocus = function (e) {
+   document.getElementById("id_box").classList.add("focus");
+};
+document.getElementById("id_text").onblur = function (e) {
+   document.getElementById("id_box").classList.remove("focus");
+};
+//비밀번호 포커스 이벤트
+document.getElementById("pw_text").onfocus = function (e) {
+   document.getElementById("pw_box").classList.add("focus");
+};
+document.getElementById("pw_text").onblur = function (e) {
+   document.getElementById("pw_box").classList.remove("focus");
+};
