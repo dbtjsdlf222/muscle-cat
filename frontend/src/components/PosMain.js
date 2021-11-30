02e382876abb757918b19a458568de17b8e3552f
kimjaebeom <jabum2000@naver.com>
2021-11-30 21:07:39 +0900

    버튼클릭

--- frontend/src/components/PosMain.js
+++ frontend/src/components/PosMain.js
@@ -1,9 +1,14 @@
 import Styles from '../css/PosMain.module.css'
+import {useState} from 'react';
 import Mini from '../minion.png'
 
 
 
 function Posmain(){
+    var [membership,tetete] = useState('')
+    function add(a){
+        tetete(membership+a);
+            }
     return(
         <div className={Styles.frame}>
             <div className={Styles.advertisement}>
@@ -11,29 +16,31 @@ function Posmain(){
             </div>
             <div className={Styles.number}>
                 <table>
+                    <tbody>
                     <tr>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
+                        <td onClick={()=>add(1)}>1</td>
+                        <td onClick={()=>add(2)}>2</td>
+                        <td onClick={()=>add(3)}>3</td>
                     </tr>
                     <tr>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
+                        <td onClick={()=>add(4)}>4</td>
+                        <td onClick={()=>add(5)}>5</td>
+                        <td onClick={()=>add(6)}>6</td>
                     </tr>
                     <tr>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
+                        <td onClick={()=>add(7)}>7</td>
+                        <td onClick={()=>add(8)}>8</td>
+                        <td onClick={()=>add(9)}>9</td>
                     </tr>
                     <tr>
                         <td>확인</td>
-                        <td>0</td>
-                        <td>취소</td>
+                        <td onClick={()=>add(0)}>0</td>
+                        <td onClick={()=>tetete('')}>취소</td>
                     </tr>
                     <tr>
-                        <td colSpan={3}><input type='text'/></td>
+                        <td colSpan={3}><input type='submit' value={membership}/></td>
                     </tr>
+                    </tbody>
                 </table>
 
             </div>
