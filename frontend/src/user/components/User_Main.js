77129de60e1210dd56d99e519a498885d870872d
kimjaebeom <jabum2000@naver.com>
2021-12-07 21:29:06 +0900

    회원사이트 메인 시작

--- frontend/src/user/components/User_Main.js
+++ frontend/src/user/components/User_Main.js
@@ -3,27 +3,53 @@ import logo from "../../logo_white.png"
 import styles from "../css/User_Main.module.css";
 
 
-export default function User_Main(){
+export default function User_Main() {
 
-    return(
-        <div >
+    return (
+        <div>
             <div className={styles.hader}>
                 <ul>
                     <div className={styles.logo_btn}>
-                        <Link to={'/'}><img src={ logo } className={ styles.logo }/></Link>
+                        <Link to={'/'}><img src={logo} className={styles.logo}/></Link>
                     </div>
+                    <div className={styles.menu}>
                     <li>
-                        <a>메뉴2</a>
+                        <a>머슬캣</a>
+                        <ul>
+                            <li>센터소개</li>
+                            <li>트레이너소개</li>
+                            <li>지점찾기</li>
+                        </ul>
                     </li>
                     <li>
-                        <a>메뉴3</a>
+                        <a>회원공간</a>
+                        <ul>
+                            <li>출석현황</li>
+                            <li>pt신청</li>
+                            <li>주센터 입장현황</li>
+                        </ul>
                     </li>
                     <li>
-                        <a>메뉴4</a>
+                        <a>게시판</a>
+                        <ul>
+                            <li>공지사항</li>
+                            <li>자유게시판</li>
+                            <li>진행중인 이벤트</li>
+                            <li>건의사항</li>
+                        </ul>
                     </li>
                     <li>
-                        <a>메뉴5</a>
+                        <a>운동꿀팁</a>
+                        <ul>
+                            <li>웨이트</li>
+                            <li>식단</li>
+                            <li>스트레칭</li>
+                        </ul>
                     </li>
+                    <li>
+                        <a><button>로그인</button></a>
+                    </li>
+                    </div>
                 </ul>
 
             </div>
