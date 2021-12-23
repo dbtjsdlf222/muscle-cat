6282206ca48f0e30c717cb18e40806d8fba891bb
You_Sun_il <gulgulcomnet@gmail.com>
2021-12-23 17:56:46 +0900

    OKOK

--- src/main/java/com/ncs7/musclecat/controller/main/TrainerController.java
+++ src/main/java/com/ncs7/musclecat/controller/main/TrainerController.java
@@ -1,6 +1,6 @@
 package com.ncs7.musclecat.controller.main;
 
-import com.ncs7.musclecat.model.UserModel;
+import com.ncs7.musclecat.model.TrainerModel;
 import com.ncs7.musclecat.repository.CenterRepository;
 import com.ncs7.musclecat.repository.FranchiseRepository;
 import com.ncs7.musclecat.repository.TrainerRepository;
@@ -10,13 +10,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/center")
+@RequestMapping("/trainer")
 @CrossOrigin(origins = "http://localhost:3000")
 @Slf4j
-public class CenterController {
+public class TrainerController {
 
     @Autowired UserRepository userRepository;
     @Autowired CenterRepository centerRepository;
@@ -24,13 +27,10 @@ public class CenterController {
     @Autowired TrainerRepository trainerRepository;
 
     @PostMapping("/join")
-    public ResponseEntity<Boolean> joinCenter(AllInfoVO vo) {
-        log.info("센터 회원가입");
-        log.debug(vo.getCenterModel().getName());
-        log.debug(vo.getFranchiseModel().getId()+" | "+vo.getFranchiseModel().getPw());
-        log.debug("qwdqdqwd");
-        centerRepository.save(vo.getCenterModel());
-        franchiseRepository.save(vo.getFranchiseModel());
+    public ResponseEntity<Boolean> joinCenter(TrainerModel trainerModel) {
+        log.info("Trainer 회원가입");
+        log.debug(trainerModel.getName());
+        trainerRepository.save(trainerModel);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
